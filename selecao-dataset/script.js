153f514eca570af7924509a75fb7bb9971da08d7
eliezerrb <eliezerrezende@outlook.com>
2023-01-27 08:10:02 -0300

    Dataset

---
+++ selecao-dataset/script.js
@@ -0,0 +1,6 @@
+// Dataset do elemento é um obj que vai ter todos os atributos que foi definido no HTML com data- alguma coisa
+
+// Selecionando por atributo, tbem funciona no CSS
+const card1 = document.querySelector('[data-product-id="32"]');
+
+console.log(card1);
